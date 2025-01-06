1bc9325518d89f61604040b22e82e17bb04b0825
Srikanth Sridharan <srikanth.sridharan36@gmail.com>
2025-01-06 13:08:28 +0530

    Privacy Policy

---
+++ app/(layout-eleven)/privacy-policy/page.jsx
@@ -0,0 +1,85 @@
+export const metadata = {
+  title: "Sofax || Responsive Next.js Template Terms and Condition Page",
+  description: "Sofax || Responsive Next.js Template",
+};
+function PrivacyPolicyPage() {
+  return (
+    <section className="sofax-section-padding5">
+      <div className="container">
+        <div className="sofax-default-content  mb-40">
+          <h2>Privacy Policy</h2>
+          <p>
+            This Privacy Policy outlines how Deansfort Technologies (“we,” “us,”
+            or “our”) safeguards the privacy of its customers, partners, and
+            visitors when using our services, including website development,
+            digital marketing, and AI technology. It describes how we collect,
+            use, disclose, and protect personal information obtained through our
+            website and services.
+          </p>
+        </div>
+        <div className="sofax-career-details-content terms-condition mb-40">
+          <h3>Consent and Information Collection:</h3>
+          <p>
+            By using our website, visitors consent to the terms outlined in this
+            Privacy Policy. We collect personal information, such as names,
+            email addresses, phone numbers, and company details, when users fill
+            out forms, register for accounts, or directly contact us. This
+            information may also include the content of messages or attachments
+            provided to us.
+          </p>
+        </div>
+        <div className="sofax-career-details-content terms-condition mb-40">
+          <h3>Purpose of Information Usage:</h3>
+          <p>
+            We utilize the collected data to deliver and improve our services,
+            personalize user experiences, process requests, and communicate
+            updates. Information is also analyzed for trends, traffic
+            monitoring, and site administration.
+          </p>
+        </div>
+        <div className="sofax-career-details-content terms-condition mb-40">
+          <h3>Advertising and Third-Party Services:</h3>
+          <p>
+            Deansfort Technologies partners with third-party advertisers who may
+            use cookies, JavaScript, and web beacons to track visitors and
+            customize ads. Google’s DART cookies are used for advertising but
+            can be disabled via Google’s Ad Policy settings. We encourage users
+            to review third-party privacy policies for more information.
+          </p>
+        </div>
+        <div className="sofax-career-details-content terms-condition mb-40">
+          <h3>GDPR and CCPA Rights:</h3>
+          <p>
+            We prioritize protecting children’s privacy and do not knowingly
+            collect data from those under 13 years old. Parents and guardians
+            are encouraged to monitor their children’s online activity. Any
+            concerns about children’s data should be reported to us for prompt
+            removal.
+          </p>
+        </div>
+        <div className="sofax-career-details-content terms-condition mb-40">
+          <h3>Children’s Privacy:</h3>
+          <p>
+            Users are entitled to access, rectify, delete, or restrict the
+            processing of their personal data. They may also object to data
+            processing or request data portability. Under CCPA, California
+            residents can request details about data collection, deletion, or
+            opt-out of data sales. Requests are addressed within one month of
+            submission.
+          </p>
+        </div>
+        <div className="sofax-career-details-content terms-condition mb130">
+          <h3>Contact Information:</h3>
+          <p>
+            For questions or privacy-related requests, users can contact
+            Deansfort Technologies at <a href="mailto:contact@deansfort.com">contact@deansfort.com</a>. We reserve
+            the right to update this Privacy Policy as required.
+          </p>
+        </div>
+        
+      </div>
+    </section>
+  );
+}
+
+export default PrivacyPolicyPage;
