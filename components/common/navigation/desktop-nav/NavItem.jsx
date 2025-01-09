422f8074485b460b484eb503ed5e344576df3a06
Srikanth Sridharan <srikanth.sridharan36@gmail.com>
2025-01-09 14:34:56 +0530

    navigation

--- components/common/navigation/desktop-nav/NavItem.jsx
+++ components/common/navigation/desktop-nav/NavItem.jsx
@@ -1,23 +1,30 @@
 /* eslint-disable react/prop-types */
 import Link from "next/link";
 import ArrowDown from "./ArrowDown";
-function NavItem({ dropdown, title, children, url = "" }) {
-	const urlFormated = url !== "/" ? `/${url}` : "/";
+function NavItem({ dropdown, title, children, url = "", extUrl }) {
+  const urlFormated = url !== "/" ? `/${url}` : "/";
 
-	return dropdown ? (
-		<li className="nav-item nav-item-has-children">
-			<a href="#" className="nav-link-item drop-trigger">
-				{title} <ArrowDown />
-			</a>
-			{children}
-		</li>
-	) : (
-		<li className="nav-item">
-			<Link href={urlFormated} className="nav-link-item">
-				{children}
-			</Link>
-		</li>
-	);
+  return dropdown ? (
+    <li className="nav-item nav-item-has-children">
+      <a href="#" className="nav-link-item drop-trigger">
+        {title} <ArrowDown />
+      </a>
+      {children}
+    </li>
+  ) : (
+    <li className="nav-item">
+      <Link
+        {...(extUrl && {
+          target: "_blank",
+          rel: "noopener noreferrer",
+        })}
+        href={extUrl ? extUrl : urlFormated}
+        className="nav-link-item"
+      >
+        {children}
+      </Link>
+    </li>
+  );
 }
 
 export default NavItem;
--- components/common/navigation/desktop-nav/index.jsx
+++ components/common/navigation/desktop-nav/index.jsx
@@ -5,40 +5,9 @@ import NavItem from "./NavItem";
 function DesktopMenu() {
 	return (
 		<DesktopNav>
-			<NavItem dropdown title="Demos">
-				<Dropdown>
-					<DropdownItem dropdown title="MultiPage">
-						<Dropdown noShape>
-							<DropdownItem url="multi-page/home-one">Sales Management</DropdownItem>
-							<DropdownItem url="multi-page/home-two">Cripto App</DropdownItem>
-							<DropdownItem url="multi-page/home-three">Team Management</DropdownItem>
-							<DropdownItem url="multi-page/home-four">Social Agency</DropdownItem>
-							<DropdownItem url="multi-page/home-five">Digital Agency</DropdownItem>
-							<DropdownItem url="multi-page/home-six">CRM</DropdownItem>
-							<DropdownItem url="multi-page/home-seven">AI SasS</DropdownItem>
-							<DropdownItem url="multi-page/home-eight">IT Services</DropdownItem>
-							<DropdownItem url="multi-page/home-nine">Email Marketing</DropdownItem>
-							<DropdownItem url="multi-page/home-ten">Web Hosting</DropdownItem>
-						</Dropdown>
-					</DropdownItem>
-					<DropdownItem dropdown title="OnePage">
-						<Dropdown noShape>
-							<DropdownItem url="one-page/home-one">Sales Management</DropdownItem>
-							<DropdownItem url="one-page/home-two">Cripto App</DropdownItem>
-							<DropdownItem url="one-page/home-three">Team Management</DropdownItem>
-							<DropdownItem url="one-page/home-four">Social Agency</DropdownItem>
-							<DropdownItem url="one-page/home-five">Digital Agency</DropdownItem>
-							<DropdownItem url="one-page/home-six">CRM</DropdownItem>
-							<DropdownItem url="one-page/home-seven">AI SasS</DropdownItem>
-							<DropdownItem url="one-page/home-eight">IT Services</DropdownItem>
-							<DropdownItem url="one-page/home-nine">Email Marketing</DropdownItem>
-							<DropdownItem url="one-page/home-ten">Web Hosting</DropdownItem>
-						</Dropdown>
-					</DropdownItem>
-				</Dropdown>
-			</NavItem>
+			<NavItem url="/">Home</NavItem>
 			<NavItem url="about-us">About Us</NavItem>
-			<NavItem dropdown title="Pages">
+			<NavItem dropdown title="Services">
 				<Dropdown>
 					<DropdownItem url="about-us">About Us</DropdownItem>
 					<DropdownItem url="pricing">Pricing</DropdownItem>
@@ -89,11 +58,8 @@ function DesktopMenu() {
 					</DropdownItem>
 				</Dropdown>
 			</NavItem>
-			<NavItem dropdown title="Blog">
-				<Dropdown>
-					<DropdownItem url="blog">Our Blog</DropdownItem>
-					<DropdownItem url="single-blog">Blog Details</DropdownItem>
-				</Dropdown>
+			<NavItem extUrl="https://deansfort.medium.com/">
+				Blog
 			</NavItem>
 			<NavItem url="contact-us">Contact Us</NavItem>
 		</DesktopNav>
