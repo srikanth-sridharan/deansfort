37b1d9ca12af141f4ebf698063f17d020444e8df
Srikanth Sridharan <srikanth.sridharan36@gmail.com>
2025-01-11 14:51:17 +0530

    contact free

--- components/contact-us-page/ContactInfo.jsx
+++ components/contact-us-page/ContactInfo.jsx
@@ -1,6 +1,8 @@
-import Icon1 from "@/public/images/contact/icon1.png";
-import Icon2 from "@/public/images/contact/icon2.png";
-import Icon3 from "@/public/images/contact/icon3.png";
+import Icon1 from "@/public/images/contact/response.png";
+import Icon2 from "@/public/images/contact/customer-service.png";
+import Icon3 from "@/public/images/contact/mail.png";
+import Icon4 from "@/public/images/contact/map.png";
+
 import Image from "next/image";
 function ContactInfo() {
 	return (
@@ -20,7 +22,7 @@ function ContactInfo() {
 				</div>
 				<div className="sofax-iconbox-data2">
 					<h4>Your Voice Matters to Us!</h4>
-					<p>Reach out at (+91 99420 07771), and we'll gladly assist you Monday-Friday, 9 AM to 5 PM.</p>
+					<p>Reach out at (+91 99420 07771), and we'll gladly assist you Monday-Friday, 9 AM to 5 PM IST.</p>
 				</div>
 			</div>
 			<div className="sofax-iconbox-wrap2">
@@ -30,6 +32,16 @@ function ContactInfo() {
 				<div className="sofax-iconbox-data2">
 					<h4>Got Questions? Drop Us a Line!</h4>
 					<p>Send us an email at contact@deansfort.com, and we’ll respond within 24 hours.</p>
+				</div>
+			</div>
+			<div className="sofax-iconbox-wrap2">
+				<div className="sofax-iconbox-icon2">
+					<Image src={Icon4} alt="Email Icon" />
+				</div>
+				<div className="sofax-iconbox-data2">
+					<h4>See Where the Magic Happens</h4>
+					<p><strong>Deansfort Technologies Pvt. Ltd.</strong><br />
+					Door No: 3/6, First Floor, Kondasamy Nagar,<br/> Veerapandi Road, Jothipuram,<br/> Coimbatore - 641047, Tamil Nadu, India</p>
 				</div>
 			</div>
 		</div>
