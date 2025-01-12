da6a1f70d88db09350c837e2b0cafab9f111ec61
Srikanth Sridharan <srikanth.sridharan36@gmail.com>
2025-01-12 14:06:33 +0530

    Robots Sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://deansfort.vercel.app/sitemap.xml',
+    sitemap: 'https://deansfort.com/sitemap.xml',
   }
 }
